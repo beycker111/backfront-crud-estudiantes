16e43228424f49368f8e985f747ef3d65563ca4a
Beycker <beycker_111@hotmail.com>
2021-09-14 14:00:51 -0500

    Se agregan las dos carpetas del proyecto front y back

---
+++ backend-crud-estudiante/src/main/java/com/estudiante/backendcrudestudiante/BackendCrudEstudianteApplication.java
@@ -0,0 +1,13 @@
+package com.estudiante.backendcrudestudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrudEstudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrudEstudianteApplication.class, args);
+	}
+
+}
